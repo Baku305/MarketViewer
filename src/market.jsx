aba84cb4de6675772a44897ba1a5fde836013458
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-29 09:38:31 +0200

    first commit

--- src/market.jsx
+++ src/market.jsx
@@ -145,31 +145,32 @@ export function Market() {
   const options = [
     { value: "ALL ASSETS", label: <Link to={`/`}>ALL ASSETS</Link> },
     ...multiStore.getState().asset.map((symbol) => {
-      return (
-        {
-          value: symbol.baseAsset,
-          label: <Link to={`/${symbol.baseAsset.toLowerCase()}`}>{symbol.baseAsset}</Link>,
-        }
-      );
+      return {
+        value: symbol.baseAsset,
+        label: <Link to={`/${symbol.baseAsset.toLowerCase()}`}>{symbol.baseAsset}</Link>,
+      };
     }),
   ];
 
   const subHeaderComponentMemo = useMemo(() => {
     return (
       <>
-        <label className="relative block">
-          <span className="sr-only w-10 h-10">Search</span>
-          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-            <img className="h-5 w-5 fill-slate-300" src={searchLogo} alt="searchLogo"></img>
-          </span>
-          <input
-            onKeyUp={(e) => setFilterText(e.target.value)}
-            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="Search ..."
-            type="text"
-            name="search"
-          />
-        </label>
+        <div className="flex justify-between items-center w-full">
+          {base_asset === "" | "/" ? <div className="w-fit h-full text-red flex item-center">ALL ASSETS</div> : <div className="w-fit h-full text-red flex item-center">SELECTED ASSET : {base_asset.toUpperCase()}</div>}
+          <label className="relative block">
+            <span className="sr-only w-10 h-10">Search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <img className="h-5 w-5 fill-slate-300" src={searchLogo} alt="searchLogo"></img>
+            </span>
+            <input
+              onKeyUp={(e) => setFilterText(e.target.value)}
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Search ..."
+              type="text"
+              name="search"
+            />
+          </label>
+        </div>
       </>
     );
   }, []);
@@ -194,8 +195,7 @@ export function Market() {
       );
     };
 
-    if (symbolsInfo && symbols24h) {
-      
+    if (symbolsInfo && symbols24h && symbolsPrice) {
       const MarketMap = symbolsInfo.symbols
         .filter((s) => s.status === "TRADING")
         .map(
@@ -209,10 +209,11 @@ export function Market() {
       dispatch(marketState.actions.set(MarketMap));
       setPending(false);
     }
-    
-    const a = multiStore.getState().market.map((s) => numberOfMarkets(s.baseAsset));
-    dispatch(AssetState.actions.set(filteredArray(a)));
 
+    console.log(base_asset);
+
+    //const a = multiStore.getState().market.map((s) => numberOfMarkets(s.baseAsset));
+    //dispatch(AssetState.actions.set(filteredArray(a)));
   }, [base_asset, dispatch, symbols24h, symbolsInfo, symbolsPrice]);
 
   const ExpandedComponent = ({ data }) => (
@@ -259,10 +260,10 @@ export function Market() {
 
   return (
     <div className="2xl:container mx-auto border-2 border-t-0 border-slate-900  overflow-x-hidden">
-      <Select
+      {/* <Select
         options={options}
         placeholder={base_asset === "" ? "ALL ASSETS" : base_asset.toUpperCase()}
-      />
+      /> */}
       <DataTable
         columns={columns}
         data={filteredItems}
