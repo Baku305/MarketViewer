917ad480789ca816b71bb9beb81bda397759f894
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-29 12:25:07 +0200

    fix search height

--- src/market.jsx
+++ src/market.jsx
@@ -162,19 +162,20 @@ export function Market() {
             </span>
             <input
               onKeyUp={(e) => setFilterText(e.target.value)}
-              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
               placeholder="Search ..."
               type="text"
               name="search"
             />
           </label>
-          {multiStore.getState().asset.length !== 0 && (
+          <div className="h-full">
             <Select
-            className="w-40"
+            className="w-40 h-full sm:text-sm "
               options={options}
               placeholder={base_asset === "" ? "ALL ASSETS" : base_asset.toUpperCase()}
             />
-          )}
+          </div>
+          
         </div>
       </>
     );
