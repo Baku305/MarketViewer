39cb1cca9fcb170aacd41d2d4ade047fe78f294a
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-08-29 10:28:54 +0200

    fix navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,22 +14,23 @@ export function Navbar() {
 
 
 
-  return (
-    <div className="wrapper flex justify-between h-14 p-1 w-full bg-slate-900" onClick={handleClick} >
-      <div className=" logoWrapper flex w-full ">
-        <img src={appLogo} alt="h-max" />
-      </div>
-      <div className="menuWrapper flex w-10 w-full">
-        <img src={burgerMenu} alt="burgerMenu" className="animate-pulse h-full" />
-        {
-         isShown && (
-         <div className="listWrapper flex flex-col absolute w-fit right-0 rounded-l-md z-10 bg-slate-900 text-white overflow-hidden">
-         <Link to = "/"><div className=" hover:bg-slate-500 w-full h-full p-3 pr-20">Markets</div> </Link>
-         <Link to = "/assets"><div className=" hover:bg-slate-500 w-full h-full p-3 pr-20">Assets</div> </Link>
-       </div>)
-        }
-      </div>
+ return (
+  <div
+    className="wrapper flex justify-between h-14 w-full bg-slate-900"
+    onClick={handleClick}
+  >
+    <div className=" logoWrapper flex w-full ">
+      <img src={appLogo} alt="h-max" className="p-1" />
+      <div className="listWrapper flex flex-row w-fit bg-slate-900 text-white">
+          <Link to="/">
+            <div className=" hover:bg-slate-500 w-full h-full pr-10 pl-10 flex items-center">Markets</div>{" "}
+          </Link>
+          <Link to="/assets">
+            <div className=" hover:bg-slate-500 w-full h-full pr-10 pl-10 flex items-center">Assets</div>{" "}
+          </Link>
+        </div>
     </div>
-  );
+  </div>
+);
 }
 
